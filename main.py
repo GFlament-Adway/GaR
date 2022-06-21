c95b08ae0e0e89f60737c6cfa69553ad027fa89f
guillaume.flament_ad <guillaume.flament@dway-conseil.com>
2022-06-21 08:48:36 +0200

    Last change

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from statsmodels.regression.quantile_regression import QuantReg
 
 
 if __name__ == "__main__":
-    countries = ["Belgium", "France", "United States"]
+    countries = ["Belgium", "France", "United States", "Sweden", "Italy"]
     energy = {country : utils.countries_energy(country=country)["country"][country] for country in countries}
 
     gdp = {country : utils.get_gdp(country) for country in countries}
@@ -12,23 +12,18 @@ if __name__ == "__main__":
 
     n = len(energy["United States"])
     p = 11
-
-
-    diff_exergy = utils.diff([energy["United States"][1971 + k]["exergy"] for k in range(n)])
-    diff_exergy = diff_exergy - np.mean(diff_exergy)
+    print(gdp.keys())
+    country = "France"
+    diff_exergy = utils.diff([energy[country][1971 + k]["exergy"] for k in range(n)])
     res = []
-    gdp_lag = gdp["United States"][(p):(n + p - 1)] - np.mean(gdp["United States"][(p):(n + p - 1)])
-    gdp = gdp["United States"][(p+1):(n + p)]- np.mean(gdp["United States"][(p+1):(n + p)])
+    gdp_lag = gdp[country][(p):(n + p-1)]
+    gdp = gdp[country][(p+1):(n + p)]
     res_2 = []
     for q in range(1, 10):
-        model = QuantReg(endog = gdp_lag, exog = diff_exergy).fit(q=q/10)
+        model = QuantReg(endog = gdp, exog = diff_exergy).fit(q=q/10)
         model_2 = QuantReg(endog = gdp, exog= gdp_lag).fit(q=q/10)
         res += [model.params]
         print(model.summary())
         res_2 += [model_2.params]
 
-    plt.figure()
-    plt.plot(diff_exergy, label="Exergy")
-    plt.plot([k+1 for k in range(len(gdp))], gdp, label="GDP forward")
-    plt.legend()
-    plt.show()
+
